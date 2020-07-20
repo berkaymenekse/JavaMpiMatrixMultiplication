35982087f4c118c100e3da2f2f1a61085803c28e
berkaymenekse <berkaymenekse15@gmail.com>
2020-07-21 01:11:28 +0300

    Matrices writed to its own txt file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -25,17 +28,36 @@ public class Main {
         char[][] A = new char[N][16];
         char[][] B = new char[16][N];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 16; j++) {
-                A[i][j] = (char) random.nextInt(255);
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("A.txt"));
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < 16; j++) {
+                    A[i][j] = (char) random.nextInt(255);
+                    bw.write(A[i][j]);
+                }
             }
-        }
+            bw.flush();
 
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < N; j++) {
-                B[i][j] = (char) random.nextInt(255);
-            }
+        } catch (IOException ex) {
+            System.out.println("A problem on writing to text file, " + ex.toString());
         }
+        ;
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("B.txt"));
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < N; j++) {
+                    B[i][j] = (char) random.nextInt(255);
+                    bw.write(B[i][j]);
+                }
+            }
+            bw.flush();
+
+        } catch (IOException ex) {
+            System.out.println("A problem on writing to text file, " + ex.toString());
+        }
+        ;
 
     }
+
 }
