97918a39e058495fe098b31e3d08d32c34f63000
berkaymenekse <berkaymenekse15@gmail.com>
2020-07-24 14:55:05 +0300

    Serial Matrix Multiplication and C output

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println("In a nano duration, Nx16 and 16xN matrices would be created, please enter N \n Note N must be greater than zero");
         Scanner scanner = new Scanner(System.in);
 
+        //Output: Nx16 16xN = NxN
+
         do {
             N = scanner.nextInt();
             if (N > 0) invalidN = false;
@@ -23,41 +25,95 @@ public class Main {
             }
         } while (invalidN);
 
-        System.out.println("yey you did N is " + N);
+        System.out.println("Entered N is " + N);
 
-        char[][] A = new char[N][16];
-        char[][] B = new char[16][N];
+        int[][] A = new int[N][16];
+        int[][] B = new int[16][N];
 
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter("A.txt"));
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < 16; j++) {
-                    A[i][j] = (char) random.nextInt(255);
+                    A[i][j] = random.nextInt(255);
                     bw.write(A[i][j]);
                 }
+                bw.write("\n");
             }
             bw.flush();
 
         } catch (IOException ex) {
-            System.out.println("A problem on writing to text file, " + ex.toString());
+            System.out.println("A problem on writing A to text file, " + ex.toString());
         }
-        ;
+
 
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter("B.txt"));
             for (int i = 0; i < 16; i++) {
                 for (int j = 0; j < N; j++) {
-                    B[i][j] = (char) random.nextInt(255);
+                    B[i][j] = random.nextInt(255);
                     bw.write(B[i][j]);
                 }
+                bw.write("\n");
             }
             bw.flush();
 
         } catch (IOException ex) {
-            System.out.println("A problem on writing to text file, " + ex.toString());
+            System.out.println("A problem on writing B to text file, " + ex.toString());
         }
-        ;
+
+        int[][] C = serialMultiMatrix(A, B);
+        printMatrix(C, "C");
+
+
+        String stringforPrint = "";
+
+  /*      for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < C[0].length; j++) {
+                stringforPrint += String.valueOf(C[i][j]) + " ";
+            }
+            stringforPrint += "\n";
+        }
+
+        System.out.println("The matrix(C) has " + C.length + "x" + C[0].length);
+        System.out.println(stringforPrint);
+        */
 
     }
+
+
+    public static int[][] serialMultiMatrix(int[][] matrix1, int[][] matrix2) {
+        if (matrix1[0].length != matrix2.length) throw new IllegalArgumentException();
+        int[][] C = new int[matrix1.length][matrix2[0].length];
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int k = 0; k < matrix1[0].length; k++) {
+                    C[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return C;
+    }
+
+    public static void printMatrix(int[][] matrix, String filename) {
+        try {
+            Random random = new Random();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(filename + ".txt"));
+            int matrixRow = matrix.length;
+            int matrixcolumn = matrix[0].length;
+
+            for (int i = 0; i < matrixRow; i++) {
+                for (int j = 0; j < matrixcolumn; j++) {
+                    bw.write(matrix[i][j]);
+                }
+                bw.write("\n");
+            }
+            bw.flush();
+
+        } catch (IOException ex) {
+            System.out.println("A problem on writing " + filename + " to text file, " + ex.toString());
+        }
+    }
+
 
 }
